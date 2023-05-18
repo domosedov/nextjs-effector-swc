dc53981703195a65e6f2c3f27cee471010431490
Aleksandr Grigorii <domosedov.dev@gmail.com>
2023-05-18 16:14:04 +0300

    add attached query example

--- src/app/model.ts
+++ src/app/model.ts
@@ -1,5 +1,5 @@
 import { createCounter } from "@/shared/factories/create_counter";
-import { createQuery } from "@farfetched/core";
+import { attachOperation, createQuery } from "@farfetched/core";
 import { createEvent, createStore, sample } from "effector";
 import { snapshot } from "patronum";
 
@@ -16,11 +16,15 @@ sample({
 
 export const q1 = createQuery({
   handler: async () => "foo",
+  name: "query-1",
 });
 
 export const q2 = createQuery({
   handler: async () => "foo",
+  name: "query-2",
 });
+
+export const q3 = attachOperation(q1);
 
 export const $snap1 = snapshot({ source: $count });
 export const $snap2 = snapshot({ source: $count });
@@ -29,6 +33,6 @@ export const c1 = createCounter({ initialCount: 10 });
 export const c2 = createCounter();
 
 console.log($count.sid);
-console.log("queries sids: ", q1.$data.sid, q2.$data.sid);
+console.log("queries sids: ", q1.$data.sid, q2.$data.sid, q3.$data.sid);
 console.log("snapshot sids: ", $snap1.sid, $snap2.sid);
 console.log("factory sids: ", c1.$count.sid, c2.$count.sid);
